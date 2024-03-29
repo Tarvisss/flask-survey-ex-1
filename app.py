fb5fbde5613264eeff90259d52a79cc2f8dd4fd8
tarvis <tarvis@gamil.com>
2024-03-28 21:01:26 -0500

    added sessions

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey as survey
+from surveys import surveys
 RESPONSES_KEY = "responses"
+CURRENT_SURVEY_KEY = 'current_survey'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "never-tell!"
+app.config['SECRET_KEY'] = "Awesome-sauce"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 
 @app.route("/")
-def show_survey_start():
-    """Select a survey."""
+def show_pick_survey_form():
+    """Show pick a survey form."""
 
-    return render_template("survey_start.html", survey=survey)
+    return render_template("pick-survey.html", surveys=surveys)
+
+@app.route("/", methods = ["POST"])
+def pick_survey():
+    """This is where youll pick which survey you want to take"""
+
+    survey_id = request.form['survey_code']
+
+    if request.cookies.get(f"completed_{survey_id}"):
+        return render_template('already-complete.html')
+    
+    survey = surveys[survey_id]
+    session[CURRENT_SURVEY_KEY] = survey_id
+
+    return render_template("survey_start.html", survey = survey)
 
 
 @app.route("/begin", methods=["POST"])
@@ -32,11 +47,15 @@ def handle_question():
 
     # get the response choice
     choice = request.form['answer']
+    text = request.form.get("text", "")
+
+    responses = session[RESPONSES_KEY]
+    responses.append({"choice" : choice, "text": text})
 
     # add this response to the session
-    responses = session[RESPONSES_KEY]
-    responses.append(choice)
     session[RESPONSES_KEY] = responses
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
 
     if (len(responses) == len(survey.questions)):
         # They've answered all the questions! Thank them.
@@ -50,6 +69,8 @@ def handle_question():
 def show_question(qid):
     """Display current question."""
     responses = session.get(RESPONSES_KEY)
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
 
     if (responses is None):
         # trying to access question page too soon
@@ -65,12 +86,21 @@ def show_question(qid):
         return redirect(f"/questions/{len(responses)}")
 
     question = survey.questions[qid]
+
     return render_template(
         "question.html", question_num=qid, question=question)
 
 
 @app.route("/complete")
 def complete():
-    """Survey complete. Show completion page."""
+    """Thank user and list responses"""
 
-    return render_template("completion.html")
+    survey_id = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_id]
+    responses = session[RESPONSES_KEY]
+
+    html = render_template("completion.html", survey = survey, responses = responses)
+
+    response = make_response(html)
+    response.set_cookie(f"completed_{survey_id}", "yes", max_age = 60)
+    return response
